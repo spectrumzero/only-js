97e71eb1a99849ca7a7aeb940329c8804be4bbd3
Spectrumzero <lmhspecter@gmail.com>
2024-09-24 17:58:36 +0800

    from Style to Ninja

---
+++ chap2_codeQuality/src/comments.js
@@ -0,0 +1,11 @@
+/*  NOTE:
+ *  1. If the code is so unclear that it requires a comment, then maybe it should be rewritten instead.
+ *  2. Sometimes it is benifical to replace a code piece with a function, or to create functions to replace comments.
+ *     Functions themselves tell what is going on so there is nothing to comment.
+ *  3. Explanatory comments are usually bad, but we still need them to provide more complex information sometimes.
+ *  4. Comments that provide high-level overview of components, how they interact, what is the control flow in various situations are acceptable.
+ *     Comments that document function parameters and usage are also acceptable. (Recommended: JSDOC for JavaScript, Doxygen for Cpp)
+ *     Comments that explain why is the task solved exactly this way rather than that way are also important.
+ *     If the code has anything subtle and counter-intuitive, it is definitely worth commenting.
+ *     AVOID commenting solely to explain how the code works and what it does. This information should be clear from the code or function itself.
+ *  */
---
+++ chap2_codeQuality/src/debugging.js
@@ -0,0 +1,17 @@
+function hello(name) {
+  let phrase = `Hello, ${name}!`;
+
+  debugger;
+
+  say(phrase);
+}
+
+function say(phrase) {
+  alert(`** ${phrase} **`);
+}
+
+function log() {
+  for (let i = 0; i < 5; i++) {
+    console.log("value", i);
+  }
+}
