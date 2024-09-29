9db24ac173c3583c61c8074cc9d28070668c21ea
Spectrumzero <lmhspecter@gmail.com>
2024-09-29 15:18:49 +0800

    Chap2 finished

---
+++ chap2_codeQuality/src/polyfills.js
@@ -0,0 +1,3 @@
+/* Just don’t forget to use a transpiler (if using modern syntax or operators which are not well-supported by current js engines)
+ * and polyfills (to add functions that may be missing).
+ * They’ll ensure that the code works.*/
